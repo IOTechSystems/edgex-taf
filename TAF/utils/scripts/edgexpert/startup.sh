09f27779ea3e2916324f6787b3480c66ba6b69af
weichou <weichou1229@gmail.com>
2020-01-14 16:51:53 +0800

    Add edgexpert deploy script
    Signed-off-by: weichou <weichou1229@gmail.com>

---
+++ TAF/utils/scripts/edgexpert/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DS_PROFILE=docker
+CONF_DIR=/custom-config
+REGISTRY_URL=consul://edgex-core-consul:8500
+
+
+docker run --rm -v ~/.docker/config.json:/root/.docker/config.json -v /var/run/docker.sock:/var/run/docker.sock \
+        -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR}/TAF/utils/scripts/edgexpert \
+        --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE}  \
+        --env DS_PROFILE=${DS_PROFILE} --env CONF_DIR=${CONF_DIR} --env REGISTRY_URL=${REGISTRY_URL} \
+        iotechsys/dev-testing-edgexpertcli:1.6.0.dev \
+        license install "${WORK_DIR}/edgexpertLicense/IoTech_42_license.lic"
+
+docker run --rm -v ~/.docker/config.json:/root/.docker/config.json -v /var/run/docker.sock:/var/run/docker.sock \
+        -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR}/TAF/utils/scripts/edgexpert \
+        --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE}  \
+        --env DS_PROFILE=${DS_PROFILE} --env CONF_DIR=${CONF_DIR} --env REGISTRY_URL=${REGISTRY_URL} \
+        iotechsys/dev-testing-edgexpertcli:1.6.0.dev \
+        up
